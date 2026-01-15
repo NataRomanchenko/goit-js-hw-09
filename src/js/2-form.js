3a1f1a357f77178314090808b42cce9051fceb36
Nataliia_Romanchenko <rnataliia36gmail.com>
2026-01-15 12:20:50 +0200

    Complete goit-js-hw-09

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,34 @@
-console.log("2-form.js підключений");
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+if (savedData) {
+  formData = savedData;
+  form.email.value = savedData.email || '';
+  form.message.value = savedData.message || '';
+}
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+
+  formData[name] = value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: '', message: '' };
+  form.reset();
+});
